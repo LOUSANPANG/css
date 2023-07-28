6c33fd700cbc118dcdab234f6dd53f8cd4cc69d2
LOUSANPANG <1271255653@qq.com>
2023-07-28 12:04:42 +0800

    day day up

---
+++ .vitepress/config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "fe-css",
+  description: "animation & styles & base components",
+
+  base: "/fe-css/",
+  srcDir: "./views",
+  srcExclude: ['README.md'],
+  ignoreDeadLinks: true,
+  markdown: {
+    lineNumbers: false
+  },
+  vite: {},
+  vue: {},
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    outline: 'deep',
+    outlineTitle: '',
+    search: {
+      provider: 'local'
+    },
+
+    nav: [
+      { text: 'Animation', link: '/animation/progress' },
+      { text: 'Style', link: '/style/reset' },
+      { text: 'Component', link: '/component/skeleton' },
+    ],
+
+    sidebar: {
+      '/animation/': [
+        { text: '进度条', link: '/animation/progress' }
+      ],
+      '/style/': [
+        { text: '重置', link: '/style/reset' }
+      ],
+      '/component/': [
+        { text: '骨架屏', link: '/component/skeleton' }
+      ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/LOUSANPANG/fe-css' }
+    ],
+
+    footer: {
+      message: '',
+      copyright: ''
+    }
+  }
+})
